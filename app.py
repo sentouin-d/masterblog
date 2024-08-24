a00f69f688bfd0ce22e1dc60da38c6700a47b47d
sentouin-d <gnerkus@protonmail.com>
2024-08-24 09:55:55 +0000

    add page to create a new blog post

--- app.py
+++ app.py
@@ -3,9 +3,21 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
+from flask import Flask, render_template, request
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+def index():
+    with open('mock.json') as mock_data:
+        blog_posts = json.load(mock_data)
+
+    return render_template('index.html', posts=blog_posts)
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        pass
+
+    return render_template("add.html")
